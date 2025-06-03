9f25e89a0d9af0a031eecedd7cd392a83e325666
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-06-03 13:47:39 +0200

    Initial version of SimpleWebApp generated by generator-jhipster@8.11.0

---
+++ WebApp/src/main/webapp/app/entities/student-app/student-app.model.ts
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs/esm';
+import { ICourseApp } from 'app/entities/course-app/course-app.model';
+import { Gender } from 'app/entities/enumerations/gender.model';
+
+export interface IStudentApp {
+  id: number;
+  firstName?: string | null;
+  lastName?: string | null;
+  birthDate?: dayjs.Dayjs | null;
+  gender?: keyof typeof Gender | null;
+  email?: string | null;
+  registrationDate?: dayjs.Dayjs | null;
+  courses?: Pick<ICourseApp, 'id'>[] | null;
+}
+
+export type NewStudentApp = Omit<IStudentApp, 'id'> & { id: null };
